0eff52791683e04ef4c5823e2bee6c8af55c477c
Derek Andre <derek@glassmantechnology.com>
2025-06-11 11:13:32 -0500

    files

--- src/app/canvas/route.ts
+++ src/app/canvas/route.ts
@@ -9,12 +9,41 @@ export async function GET() {
     <html>
     <head>
     <script type="text/javascript" src="${sdk}"></script>
+    <script type="text/javascript">
+        function dragOverHandler(ev) {
+            ev.preventDefault();
+        }
+        function dropHandler(ev) {
+            ev.preventDefault();
+
+            let message = '';   
+
+            if (ev.dataTransfer.items) {                
+                [...ev.dataTransfer.items].forEach((item, i) => {      
+                    if (item.kind === "file") {
+                        const file = item.getAsFile();  
+                        message += "<div>File: " + file.name + " Size: " + file.size + " bytes</div>";        
+                    }
+                });
+            } else {                
+                [...ev.dataTransfer.files].forEach((file, i) => {
+                    message += "<div>File: " + file.name + " Size: " + file.size + " bytes</div>";
+                });
+            }
+
+            document.getElementById("drop_zone_message").innerHTML = message;
+        }
+    </script>
     </head>
     <body>
-    <h1>Hi Derek!</h1>
-    <script type="text/javascript">
-    
-    </script>
+    <h2>Hi Derek!</h2>
+    <div
+    id="drop_zone"
+    style="border: 5px solid black;width:200px;height:100px;"
+    ondrop="dropHandler(event);"
+    ondragover="dragOverHandler(event);">
+        <div id="drop_zone_message">Drag one or more files here.</div>
+    </div>    
     </body>
     </html>`;
     
